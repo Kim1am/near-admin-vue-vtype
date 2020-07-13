534b6bbc7cc4f83041524817faf3794a46cdcfe8
Kim1am <ykimlam@163.com>
2020-07-13 12:49:51 +0800

    prettierrc init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false, // 行位是否使用分号，默认为true
+  trailingComma: 'none', // 是否使用尾逗号，有三个可选值"<none|es5|all>"
+  singleQuote: true, // 字符串是否使用单引号，默认为false，使用双引号
+  tabWidth: 2, // 一个tab代表几个空格数
+  useTabs: false, // 启用tab缩进
+  bracketSpacing: true, // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+}
